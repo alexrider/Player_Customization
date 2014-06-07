3da0c95994315b8aebf39ff70b4a3041849a6cbc
Austin Williamson <contact@austinwilliamson.name>
2014-06-06 18:58:19 -0600

    Status: still broken (somewhat - Engine issues)
    Fixed a lot of things, though the minetest engine needs better alpha
    transparency support.

--- clothing/init.lua
+++ clothing/init.lua
@@ -30,15 +30,15 @@ recipe = {
 })
 minetest.register_tool("clothing:shirt_"..k, {
 description = k.." shirt",
-inventory_image = "clothing_inv_shirt_"..k.."_overlay.png^clothing_shirt_inv_white.png",
-texture = "clothing_shirt_"..k.."_overlay.png^clothing_shirt_white.png",
+inventory_image = "clothing_inv_shirt_white.png^clothing_inv_shirt_"..k.."_overlay.png",
+texture = "clothing_shirt_white.png^clothing_shirt_"..k.."_overlay.png",
 groups = {clothing=1, no_preview=1},
 })
 
 minetest.register_tool("clothing:pants_"..k, {
 description = k.." pants",
-inventory_image = "clothing_inv_pants_"..k.."_overlay.png^clothing_pants_inv_white.png",
-texture = "clothing_pants_"..k.."_overlay.png^clothing_inv_pants_white.png",
+inventory_image = "clothing_inv_pants_white.png^clothing_inv_pants_"..k.."_overlay.png",
+texture = "clothing_pants_white.png^clothing_pants_"..k.."_overlay.png",
 groups = {clothing=1, no_preview=1},
 })
 end
@@ -78,33 +78,10 @@ recipe = {
 })
 
 -- Stu's Stuff
-minetest.register_craftitem("clothing:hindsight_shirt", {
-	description = "Captain Hindsight Shirt",
-	inventory_image = "clothing_inv_hindsight_shirt.png",
-	groups = {clothing=1},
-})
 
-minetest.register_craftitem("clothing:hindsight_pants", {
-	description = "Captain Hindsight Pants",
-	inventory_image = "clothing_inv_hindsight_pants.png",
-	groups = {clothing=1},
-})
-
-minetest.register_craftitem("clothing:hindsight_boots", {
-	description = "Captain Hindsight Boots",
-	inventory_image = "clothing_inv_hindsight_boots.png",
-	groups = {clothing=1},
-})
 
 minetest.register_craftitem("clothing:hindsight_cape", {
 	description = "Captain Hindsight Cape",
 	inventory_image = "clothing_inv_hindsight_cape.png",
 	groups = {clothing=1, no_preview=1},
 })
-
-minetest.register_craftitem("clothing:paper_bag", {
-	description = "Paper Bag",
-	inventory_image = "clothing_inv_paper_bag.png",
-	groups = {clothing=1, no_preview=1},
-})
-
